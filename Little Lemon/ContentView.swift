fdd4300b5defe1ebc9f2c1f4db5e6dec1b8449cf
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-10-28 19:11:04 +0300

    Initial Commit

---
+++ Little Lemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by Qusea Saif on 10/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
