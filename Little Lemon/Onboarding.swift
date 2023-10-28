ab028ed8fb51b5b7434d69c3eb343100d86a5861
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-10-28 21:32:54 +0300

    Initial commit

---
+++ Little Lemon/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  Little Lemon
+//
+//  Created by Qusea Saif on 10/28/23.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
