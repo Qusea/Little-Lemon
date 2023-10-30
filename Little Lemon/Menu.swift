1ef557c7c9175621cde113e2ac75ca6c6e6f2039
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-10-30 23:35:39 +0300

    Added getMenuData() method

--- Little Lemon/Menu.swift
+++ Little Lemon/Menu.swift
@@ -1,10 +1,3 @@
-//
-//  Menu.swift
-//  Little Lemon
-//
-//  Created by Qusea Saif on 10/29/23.
-//
-
 import SwiftUI
 
 struct Menu: View {
@@ -29,9 +22,29 @@ struct Menu: View {
 
       }
     }
+    .onAppear {
+      getMenuData()
+    }
   }
-}
 
-#Preview {
-  Menu()
+  func getMenuData() {
+    let serverUrl = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+    let url = URL(string: serverUrl)!
+    let request = URLRequest(url: url)
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+      if let error = error {
+        print("Error: \(error.localizedDescription)")
+        return
+      }
+      guard let httpResponse = response as? HTTPURLResponse,
+            (200...299).contains(httpResponse.statusCode) else {
+        print("Error: Invalid response")
+        return
+      }
+      if let data = data {
+        print("Data: \(data)")
+      }
+    }
+    task.resume()
+  }
 }
