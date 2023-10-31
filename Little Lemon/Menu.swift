6f1c91070815e463cd0200f243f1a39439757c6a
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-10-31 04:04:52 +0300

    Save mapped menu items into the database

--- Little Lemon/Menu.swift
+++ Little Lemon/Menu.swift
@@ -1,56 +1,68 @@
+//
+//  Menu.swift
+//  Little Lemon
+//
+//  Created by Qusea Saif on 10/29/23.
+//
+
 import SwiftUI
 
 struct Menu: View {
+    @Environment(\.managedObjectContext) private var viewContext
 
-  @Environment(\.managedObjectContext) private var viewContext
-
-  var body: some View {
-    VStack {
-      Text("Little Lemon")
-        .font(.title)
-        .fontWeight(.bold)
-        .foregroundColor(Color("DarkGreen"))
-        .padding(.bottom, 20)
-      Text("Chicago")
-        .font(.subheadline)
-        .foregroundColor(Color("DarkGreen"))
-        .padding(.bottom, 20)
-      Text(
-        "Little Lemon is a restaurant that serves healthy food. We are located in Chicago and we are open from 8am to 10pm every day."
-      )
-      .font(.subheadline)
-      .foregroundColor(Color("DarkGreen"))
-      .padding(.bottom, 20)
-      List {
-
-      }
-    }
-    .onAppear {
-      getMenuData()
-    }
-  }
-
-  func getMenuData() {
-    let serverUrl = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
-    let url = URL(string: serverUrl)!
-    let request = URLRequest(url: url)
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-      if let error = error {
-        print("Error: \(error.localizedDescription)")
-        return
-      }
-      guard let httpResponse = response as? HTTPURLResponse,
-            (200...299).contains(httpResponse.statusCode) else {
-        print("Error: Invalid response")
-        return
-      }
-      if let data = data {
-        let decoder = JSONDecoder()
-        if let menuList = try? decoder.decode(MenuList.self, from: data) {
-          print(menuList)
+    var body: some View {
+        VStack {
+            Text("Little Lemon")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+            Text("Chicago")
+                .font(.subheadline)
+                .padding(.bottom, 20)
+            Text(
+                "Little Lemon is a restaurant that serves healthy food. We are located in Chicago and we are open from 8am to 10pm every day."
+            )
+            .font(.subheadline)
+            .padding(.bottom, 20)
+            List {}
         }
-      }
+        .onAppear {
+            getMenuData()
+        }
     }
-    task.resume()
-  }
+
+    func getMenuData() {
+        PersistenceController.shared.clear()
+        let serverUrl = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
+        let url = URL(string: serverUrl)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                return
+            }
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200 ... 299).contains(httpResponse.statusCode)
+            else {
+                print("Error: Invalid response")
+                return
+            }
+            if let data = data {
+                let decoder = JSONDecoder()
+                if let menuList = try? decoder.decode(MenuList.self, from: data) {
+                    for item in menuList.menu {
+                        var dish = Dish(context: viewContext)
+                        dish.title = item.title
+                        dish.image = item.image
+                        dish.price = item.price
+                        dish.category = item.category
+                        dish.dishDescription = item.description
+                    }
+					
+					try? viewContext.save()
+                }
+            }
+        }
+        task.resume()
+    }
 }
