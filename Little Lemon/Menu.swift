e3432440f8bad2abf8451a6390f885980f5eddd0
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-11-01 23:07:48 +0300

    Cleanup Menu View & add SortDescriptor

--- Little Lemon/Menu.swift
+++ Little Lemon/Menu.swift
@@ -1,10 +1,4 @@
-//
-//  Menu.swift
-//  Little Lemon
-//
-//  Created by Qusea Saif on 10/29/23.
-//
-
+import CoreData
 import SwiftUI
 
 struct Menu: View {
@@ -23,8 +17,25 @@ struct Menu: View {
                 "Little Lemon is a restaurant that serves healthy food. We are located in Chicago and we are open from 8am to 10pm every day."
             )
             .font(.subheadline)
-            .padding(.bottom, 20)
-            List {}
+            .padding()
+            FetchedObjects(sortDescriptors: buildSortDescriptors()) { (dishes: [Dish]) in
+                List(dishes) { dish in
+                    NavigationLink {
+                        DishView(dish: dish)
+                    } label: {
+                        HStack {
+                            Text("\(dish.title!) - $\(dish.price!)")
+                            Spacer()
+                            AsyncImage(url: URL(string: dish.image!)!) { image in
+                                image.resizable()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
+                        }
+                    }
+                }
+            }
         }
         .onAppear {
             getMenuData()
@@ -36,33 +47,31 @@ struct Menu: View {
         let serverUrl = "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json"
         let url = URL(string: serverUrl)!
         let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
             if let error = error {
                 print("Error: \(error.localizedDescription)")
-                return
-            }
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200 ... 299).contains(httpResponse.statusCode)
-            else {
-                print("Error: Invalid response")
                 return
             }
             if let data = data {
                 let decoder = JSONDecoder()
                 if let menuList = try? decoder.decode(MenuList.self, from: data) {
                     for item in menuList.menu {
-                        var dish = Dish(context: viewContext)
+                        let dish = Dish(context: viewContext)
                         dish.title = item.title
                         dish.image = item.image
                         dish.price = item.price
                         dish.category = item.category
                         dish.dishDescription = item.description
                     }
-					
-					try? viewContext.save()
+
+                    try? viewContext.save()
                 }
             }
         }
         task.resume()
+    }
+
+    func buildSortDescriptors() -> [NSSortDescriptor] {
+        return [NSSortDescriptor(key: "title", ascending: true, selector: #selector(NSString.localizedStandardCompare))]
     }
 }
