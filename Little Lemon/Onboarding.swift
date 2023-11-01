e96cf2cd8b05c3b0e68e18e00f69bf5918735b8d
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-11-01 23:04:52 +0300

    cleanup Onboarding view

--- Little Lemon/Onboarding.swift
+++ Little Lemon/Onboarding.swift
@@ -12,7 +12,6 @@ let emailKey = "email"
 import SwiftUI
 
 struct Onboarding: View {
-
     @State private var isLoggedIn = false
     @State private var firstName = ""
     @State private var lastName = ""
@@ -26,28 +25,23 @@ struct Onboarding: View {
                     isActive: $isLoggedIn,
                     label: {
                         EmptyView()
-                    })
-                Form {
-                    TextField("First Name", text: $firstName)
-                        .padding()
-                    TextField("Last Name", text: $lastName)
-                        .padding()
-                    TextField("Email", text: $email)
-                        .padding()
-                        .background(Color("LightGreen"))
-                        .cornerRadius(5.0)
-                        .padding(.bottom, 20)
-                }
+                    }
+                )
+
+                TextField("First Name", text: $firstName)
+                    .padding()
+                TextField("Last Name", text: $lastName)
+                    .padding()
+                TextField("Email", text: $email)
+                    .padding()
+
                 Button(action: {
-                    if !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty {
-                        // add the following code to store the values of firstName, lastName and email in UserDefaults
+                    if !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty {
                         UserDefaults.standard.set(self.firstName, forKey: firstNameKey)
                         UserDefaults.standard.set(self.lastName, forKey: lastNameKey)
                         UserDefaults.standard.set(self.email, forKey: emailKey)
-                        // add the following code to change the value of the isOnboardingCompleted key to true
                         UserDefaults.standard.set(true, forKey: "isLoggedIn")
-                        // add the following code to dismiss the registration page
-    //                    self.presentationMode.wrappedValue.dismiss()
+
                         $isLoggedIn.wrappedValue.toggle()
                     }
                 }) {
@@ -55,13 +49,11 @@ struct Onboarding: View {
                         .font(.headline)
                         .foregroundColor(.white)
                         .padding()
-                        .frame(width: 220, height: 60)
-                        .background(Color("DarkGreen"))
                 }
                 .cornerRadius(15.0)
                 .background(Color.yellow)
             }
-            .onAppear() {
+            .onAppear {
                 if UserDefaults.standard.bool(forKey: "isLoggedIn") {
                     $isLoggedIn.wrappedValue.toggle()
                 }
