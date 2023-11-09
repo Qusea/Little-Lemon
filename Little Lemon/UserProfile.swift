836e2d308dda17c19ee451f8ba6a1f26833d08e1
Qusea Saif <6491176+Qusea@users.noreply.github.com>
2023-11-10 00:53:50 +0300

    Refactored UserProfile view to use AppStorage and added input fields for personal information

--- Little Lemon/UserProfile.swift
+++ Little Lemon/UserProfile.swift
@@ -8,34 +8,70 @@
 import SwiftUI
 
 struct UserProfile: View {
-
 	@Environment(\.presentationMode) var presentationMode
-	let firstName = UserDefaults.standard.string(forKey: firstNameKey) ?? ""
-	let lastName = UserDefaults.standard.string(forKey: lastNameKey) ?? ""
-	let email = UserDefaults.standard.string(forKey: emailKey) ?? ""
+	@AppStorage(firstNameKey) var firstName = ""
+	@AppStorage(lastNameKey) var lastName = ""
+	@AppStorage(emailKey) var email = ""
 
-    var body: some View {
+	var body: some View {
 		VStack {
+			HStack(alignment: .center) {
+				LittleLemonLogo()
+			}
+			.padding()
+			Spacer()
 			Text("Personal information")
 			Image("profile-image-placeholder")
 				.resizable()
 				.frame(width: 100, height: 100)
 				.clipShape(Circle())
 				.padding(.bottom, 20)
-			Text("First Name: \(firstName)")
-			Text("Last Name: \(lastName)")
-			Text("Email: \(email)")
+			Text("First Name")
+				.fontWeight(.bold)
+			TextField("First Name", text: $firstName)
+				.textFieldStyle(.roundedBorder)
+			Text("Last Name")
+				.fontWeight(.bold)
+			TextField("Last Name", text: $lastName)
+				.textFieldStyle(.roundedBorder)
+			Text("Email")
+				.fontWeight(.bold)
+			TextField("Email", text: $email)
+				.keyboardType(.emailAddress)
+				.textFieldStyle(.roundedBorder)
+				.textInputAutocapitalization(.never)
 
+			Spacer()
 			Button(action: {
+				if !firstName.isEmpty, !lastName.isEmpty, !email.isEmpty {
+					UserDefaults.standard.set(firstName, forKey: firstNameKey)
+					UserDefaults.standard.set(lastName, forKey: lastNameKey)
+					UserDefaults.standard.set(email, forKey: emailKey)
+					print("Saved")
+				}
+			}) {
+				Text("Save")
+					.font(.headline)
+					.foregroundColor(.white)
+					.padding()
+			}
+			.background(Color.jonquil)
+			.cornerRadius(15)
+
+			Spacer()
+			Button(action: {
+				UserDefaults.standard.removeObject(forKey: firstNameKey)
+				UserDefaults.standard.removeObject(forKey: lastNameKey)
+				UserDefaults.standard.removeObject(forKey: emailKey)
 				UserDefaults.standard.set(false, forKey: "isLoggedIn")
+				print("Logged Out")
 				self.presentationMode.wrappedValue.dismiss()
 			}) {
 				Text("Logout")
 			}
+			Spacer()
 		}
-    }
-}
-
-#Preview {
-    UserProfile()
+		.autocorrectionDisabled()
+		.padding(.horizontal)
+	}
 }
